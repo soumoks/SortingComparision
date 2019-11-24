9ae2ef1e923c405301ab3fd03129f6fa8459264a
soumoks <soumoks@gmail.com>
2019-11-24 14:35:00 -0700

    Added validation logic for input arguments

--- src/SortingApp.java
+++ src/SortingApp.java
@@ -1,5 +1,59 @@
 public class SortingApp {
     public static void main(String [] args){
-        
+        String order = "";
+        int size = 0;
+        String algorithm = "";
+
+        //Contains the name of the outputfile
+        String outputFile = "";
+
+        if(args.length > 0 && args.length == 4 && validateOrder(args[0]) && validateAlgorithm(args[2])){
+            order = args[0];
+            size = Integer.parseInt(args[1]);
+            algorithm = args[2];
+            outputFile = args[3];
+            System.out.println("Argument lenght: " + args.length);
+            System.out.println("Order: " + order);
+            System.out.println("Size: " + size);
+            System.out.println("Algorithm: " + algorithm);
+            System.out.println("OutputFile: " + outputFile);
+        }
+        else{
+            System.out.println("Please correct your inputs and try again");
+        }
     }
+
+    public static boolean validateOrder(String myOrder){
+        //Possible values for order
+        String [] orderValues = {"ascending","descending","random"};
+        boolean validOrder = false;
+        for(String temp:orderValues){
+            if(temp.equals(myOrder.toLowerCase())){
+                validOrder = true;
+                break;
+            }
+            else{
+                validOrder = false;
+            }
+        }
+        return validOrder;
+    }
+
+    public static boolean validateAlgorithm(String myAlgorithm){
+        //Possible values for algorithm.
+        String [] algorithmValues = {"bubble","insertion","merge","quick"};
+        boolean validAlgorithm = false;
+        for(String temp:algorithmValues){
+            if(temp.equals(myAlgorithm.toLowerCase())){
+                validAlgorithm = true;
+                break;
+            }
+            else{
+                validAlgorithm = false;
+            }
+        }
+        return validAlgorithm;
+    }
+
+
 }
